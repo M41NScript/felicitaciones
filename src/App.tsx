3a85a7a55b629ab6bcd3215c78b97a6cbcb704a1
Juan C. Zuñiga P <jczp_020401@hotmail.com>
2023-09-25 18:18:34 -0500

    last update1

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import bg from './assets/bg.png'
+import photo from './assets/p.png'
+import flower1 from './assets/flower1.png'
+
+const flowers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+function App() {
+
+  const jumpingFlowers = () => {
+
+  }
+
+  return (
+    <>
+      <img src={bg} className='bg' />
+      <img src={flower1} className='flower' />
+      <img src={flower1} className='flower2' />
+      <img src={flower1} className='flower3' />
+      <img src={flower1} className='flower4' />
+      <img src={flower1} className='flower5' />
+      <img src={flower1} className='flower6' />
+      <img src={flower1} className='flower7' />
+      <img src={flower1} className='flower8' />
+      <img src={flower1} className='flower9' />
+      <img src={flower1} className='flower10' />
+      <img src={flower1} className='flower delay1' />
+      <img src={flower1} className='flower2 delay1' />
+      <img src={flower1} className='flower3 delay1' />
+      <img src={flower1} className='flower4 delay1' />
+      <img src={flower1} className='flower5 delay1' />
+      <img src={flower1} className='flower6 delay1' />
+      <img src={flower1} className='flower7 delay1' />
+      <img src={flower1} className='flower8 delay1' />
+      <img src={flower1} className='flower9 delay1' />
+      <img src={flower1} className='flower10 delay1' />
+      <img src={flower1} className='flower delay2' />
+      <img src={flower1} className='flower2 delay2' />
+      <img src={flower1} className='flower3 delay2' />
+      <img src={flower1} className='flower4 delay2' />
+      <img src={flower1} className='flower5 delay2' />
+      <img src={flower1} className='flower6 delay2' />
+      <img src={flower1} className='flower7 delay2' />
+      <img src={flower1} className='flower8 delay2' />
+      <img src={flower1} className='flower9 delay2' />
+      <img src={flower1} className='flower10 delay2' />
+      <div className='content'>
+        <p className='Title'>Feliz Cumpleaños</p>
+        <div className='windows'>
+          <div className='windowsContent'>
+            <p className='message'>¡Feliz Cumpleaños! </p>
+            <img src={photo} className='photo' />
+            <div>
+              <p className='message'>Te deseo un año lleno de amor, felicidad y alegría.</p>
+              <p className='message'> Que cada minuto esté repleto de momentos especiales y que tus sueños se hagan realidad.</p>
+              <p className='message'> ¡Que este nuevo año de vida sea el mejor hasta ahora! </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
